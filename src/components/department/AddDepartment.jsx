e271a687ebaa2faafabdaa4ba6cc4603c8bbef50
Vikash Gupta <156522608+Vkgup8986@users.noreply.github.com>
2025-12-12 17:13:20 +0530

    Update API endpoint for adding department

--- src/components/department/AddDepartment.jsx
+++ src/components/department/AddDepartment.jsx
@@ -16,7 +16,7 @@ const AddDepartment = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const response = await axios.post('http://localhost:5000/api/department/add', department, {
+      const response = await axios.post('https://employee-backend-api-ruddy.vercel.app/api/department/add', department, {
         headers: { "Authorization": `Bearer ${localStorage.getItem('token')}` }
       })
       if (response.data.success) {
